2a665891762a6568588b5520f1251001c2aa1057
unknown <shokohida_0223@yahoo.co.jp>
2025-04-09 06:35:35 +0900

    ログイン処理これからログアウト処理する

--- shoppinglist_project/app/views.py
+++ shoppinglist_project/app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import(
     TemplateView, CreateView, FormView, View
 )
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegistForm, UserLoginForm
 
 class HomeView(TemplateView):
@@ -17,6 +18,15 @@ class UserLoginView(FormView):
     template_name = 'user_login_form.html'
     form_class = UserLoginForm
     success_url = reverse_lazy('app:home')
+    
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        user = authenticate(email=email, password=password)
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+            
     
 class UserLogoutView(View):
     pass
