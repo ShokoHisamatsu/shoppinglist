84b39fef847ad25f9649ab8efdb5c48e721c3f97
unknown <shokohida_0223@yahoo.co.jp>
2025-06-03 23:17:01 +0900

    ログアウト再修正

--- shoppinglist_project/app/views.py
+++ shoppinglist_project/app/views.py
@@ -593,5 +593,5 @@ class PortfolioView(TemplateView):
                 
 def logout_then_login(request):
     logout(request)
-    return redirect(reverse_lazy('user_login'))
+    return redirect('user_login')
     
