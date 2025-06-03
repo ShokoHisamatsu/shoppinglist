d2b2c84feafd14f8912e874cedbbfe03b0595aa6
unknown <shokohida_0223@yahoo.co.jp>
2025-06-03 10:11:19 +0900

    レイアウト修正

--- shoppinglist_project/app/views.py
+++ shoppinglist_project/app/views.py
@@ -79,13 +79,13 @@ class UserLoginView(FormView):
         next_url = self.request.GET.get('next')
         return next_url if next_url else self.success_url            
     
-class UserLogoutView(View):
-    def get(self, request, *args,**kwargs):
-        return render(request, 'user_logout_form.html')
+# class UserLogoutView(View):
+#     def get(self, request, *args,**kwargs):
+#         return render(request, 'user_logout_form.html')
 
-    def post(self, request, *args, **kwargs):
-        logout(request)
-        return redirect('app:home')
+#     def post(self, request, *args, **kwargs):
+#         logout(request)
+#         return redirect('app:home')
 
 
 class StoreDeleteView(LoginRequiredMixin, DeleteView):
