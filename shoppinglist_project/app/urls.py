1ff246fc1c2225593de2bd11385244e5824b7243
unknown <shokohida_0223@yahoo.co.jp>
2025-06-03 13:48:03 +0900

    LogoutView変更

--- shoppinglist_project/app/urls.py
+++ shoppinglist_project/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, reverse_lazy
 from .views import (
     RegistUserView, HomeView, UserLoginView,
-    LogoutView, MyListView, StoreDeleteView,
+    MyListView, StoreDeleteView,
     CategoryListView, CategoryItemListView, ItemCategoryCreateView,
     CategoryAddView, ItemCheckView, ItemDeleteView, EmailChangeView,
     PortfolioView
@@ -10,7 +10,7 @@ from . import views
 from django.contrib.auth.views import (
     PasswordChangeView, PasswordChangeDoneView, PasswordResetView, 
     PasswordResetDoneView, PasswordResetConfirmView, 
-    PasswordResetCompleteView
+    PasswordResetCompleteView, LogoutView
 )
 from django.views.generic import TemplateView
 
