6ff8fdce6b2f9d5ca029d46146a48c8196568183
unknown <shokohida_0223@yahoo.co.jp>
2025-06-08 21:42:21 +0900

    アイテムに追加変更

--- shoppinglist_project/app/templatetags/form_tags.py
+++ shoppinglist_project/app/templatetags/form_tags.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter(name='add_attr')
+def add_attr(field, attr):
+    attrs = {}
+    definition = attr.split('=')
+    if len(definition) == 2:
+        attrs[definition[0]] = definition[1]
+    return field.as_widget(attrs=attrs)
