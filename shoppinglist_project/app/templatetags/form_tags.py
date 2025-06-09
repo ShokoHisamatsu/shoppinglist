76ece2dd1ecf274ee07fedb0fac15650a9d9865e
unknown <shokohida_0223@yahoo.co.jp>
2025-06-09 14:47:45 +0900

    カテゴリ名削除ボタンエラー修正

--- shoppinglist_project/app/templatetags/form_tags.py
+++ shoppinglist_project/app/templatetags/form_tags.py
@@ -13,3 +13,8 @@ def add_attr(field, attr):
     if len(definition) == 2:
         attrs[definition[0]] = definition[1]
     return field.as_widget(attrs=attrs)
+
+
+@register.filter
+def dict_get(dict_obj, key):
+    return dict_obj.get(key)
