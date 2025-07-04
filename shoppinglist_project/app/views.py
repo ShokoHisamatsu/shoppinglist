8a9b99d71e5243db5995708119e2aa4716a3a821
unknown <shokohida_0223@yahoo.co.jp>
2025-07-04 23:21:21 +0900

    削除不可メッセージの誘導改善にリスト名追加

--- shoppinglist_project/app/views.py
+++ shoppinglist_project/app/views.py
@@ -664,7 +664,7 @@ def category_master_delete(request, pk):
         # 使われているリスト名を箇条書き
         msg = format_html(
             '「{cat}」のカテゴリは、以下のショッピングリストで使用されています。<br>'
-            '{llist}<br>'
+            '{list}<br>'
             '削除するには、該当するリスト画面でこのカテゴリを削除してください。',
             # ↓ まず箇条書き HTML（位置引数）
             format_html_join('<br>', '・ {}', ((name,) for name in linked_qs)),
