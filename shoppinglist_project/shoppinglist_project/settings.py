040cf0b161b48b20e15f93a21bac3026776580ab
unknown <shokohida_0223@yahoo.co.jp>
2025-07-31 08:44:58 +0900

    パスワードリセットメール修正

--- shoppinglist_project/shoppinglist_project/settings.py
+++ shoppinglist_project/shoppinglist_project/settings.py
@@ -147,7 +147,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Shopli <shoplisince2025@gmail.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = f"Shopli <{EMAIL_HOST_USER}>"
+
 
 
 LOGOUT_REDIRECT_URL = 'app:login'
